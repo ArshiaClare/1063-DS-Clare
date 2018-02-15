d6a31c7e5be0f146f0e1d5dc912eacafdafddae9
ArshiaClare <35582387+ArshiaClare@users.noreply.github.com>
2018-02-15 14:23:10 -0600

    Update print_2d.cpp

--- assignments/homework_2/print_2d.cpp
+++ assignments/homework_2/print_2d.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
+int main() {
+  int  i,j, k=1;
+int Nums[15][15];
+
+  for(i=0;i<15;i++){
+   for(j=0;j<15;j++){
+      Nums[i][j]=k;
+      k++;
+      cout<< setw(5) << k;
+   }
+   cout<< endl;
+  }
+  for (int row = 0; row< 15; row++){
+   for(int col = 0; col < 15; col++){
+    cout<< setw(5) << Nums[col][row];
+   }
+   cout << endl;
+  }
+}
